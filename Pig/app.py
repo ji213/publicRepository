3fa17da9abd1226f42c1708b57e8bdf433d43baa
ji213 <156246909+ji213@users.noreply.github.com>
2024-01-15 21:54:16 -0500

    Enhancements_Misc
    Updated game instructions and updated language during game. Also added scoreboard to assist users

--- Pig/app.py
+++ Pig/app.py
@@ -9,7 +9,19 @@ def roll():
 
     return roll_return
 
+
 ##While loop to configure num of players to start game
+
+print("The game is called 'Pig' (don't ask me I dont know why)\n Here is how it works: \n")
+print("Each player will get a turn to roll the dice, you will choose whether to roll or pass to the next player\n")
+print("Each roll of the dice adds to your score for that round. \n However, if you roll a 1 your turn will end and your score for that round will reset to 0 \n")
+print("If you survive the round, your round score will be added to your total score\n")
+print("The goal is to reach the target score first.\n If two players or more reach the target score in the same round, \n tie goes to the highest score")
+
+max_score = 20
+
+print("The target score configured for this round is ", max_score)
+
 
 while True:
     ##Input num of players
@@ -24,17 +36,19 @@ while True:
     else:
         print("Invalid input, try again")
 
-max_score = 20
+
 player_scores = [0 for _ in range(players)]
 
 round = 1
 
 while max(player_scores) < max_score:
     ##Print current round
-    print("ROUND ", round)
+    print("START OF ROUND ", round)
 
     ##Loop through player scores and display to user
-    print("Current Scores will be added here in next update")
+    print("Scoreboard:\n")
+    for i in range(players):
+        print("Player ", i+1,"- ", player_scores[i])
 
     for player_idx in range(players):
         print("\nPlayer ", player_idx + 1, "turn has just started\n")
@@ -46,12 +60,13 @@ while max(player_scores) < max_score:
         while True:
             should_roll = input("Are you ready to roll (y)? ")
             if should_roll.lower() != 'y':
+                ##if should_roll isnt a y or n output error message
                 break
 
             value = roll()
 
             if value == 1:
-                print("You rolled a one, turn ends")
+                print("Ouch! You rolled a 1, so your turn done")
                 current_score = 0
                 break
             else:
@@ -63,7 +78,6 @@ while max(player_scores) < max_score:
 
         player_scores[player_idx] += current_score
         print("Total score for player ", player_idx + 1, ": ", player_scores[player_idx])
-
 
 ##find the max score acheived to determine winner then display to user
 
